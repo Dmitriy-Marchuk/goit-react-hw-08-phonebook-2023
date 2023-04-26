cc0568cf1dda70167ad57a46eb0e0d7755095b5a
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2023-04-26 22:16:23 +0300

    fix git bug

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 export const AppContainer = styled.div`
-  height: 100vh;
+  /* height: 100vh; */
   display: flex;
-  justify-content: center;
+  /* justify-content: center; */
   flex-direction: column;
   align-items: center;
   font-size: 40px;
