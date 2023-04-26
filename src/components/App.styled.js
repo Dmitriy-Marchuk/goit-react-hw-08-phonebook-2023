6a2270d9e4eb47f182c893ff2faad4a6fdde44aa
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2023-04-26 16:51:28 +0300

    fixed height

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const AppContainer = styled.div`
-  /* height: 100%; */
+  height: 100vh;
   display: flex;
   justify-content: center;
   flex-direction: column;
