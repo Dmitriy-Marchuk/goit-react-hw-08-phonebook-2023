03b7d526b24fc97a55f37a58d19adf06c1ce3664
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2023-05-17 23:09:32 +0300

    redux refactoring

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { ContactsElement, DeleteButton } from './Contacts.styled';
+import { useSelector } from 'react-redux';
+import { getContacts, getFilter } from 'redux/selectors';
 
-const Contacts = ({ contacts, onDeleteContact }) => (
-  <ul>
-    {contacts.map(({ id, name, number }) => (
-      <ContactsElement key={id}>
-        <div>
-          <p>{name}:</p>
-          <p>{number}</p>
-        </div>
-        <DeleteButton onClick={() => onDeleteContact(id)}>
-          Delete contact
-        </DeleteButton>
-      </ContactsElement>
-    ))}
-  </ul>
-);
+const Contacts = ({ onDeleteContact }) => {
+  const contacts = useSelector(getContacts);
+  // const filter = useSelector(getFilter).filter.toLowerCase().trim();
+
+  // const filteredContacts = () => {
+  //   return contacts.filter(contact =>
+  //     contact.name.toLowerCase().includes(filter)
+  //   );
+  // };
+
+  return (
+    <ul>
+      {contacts.map(({ id, name, number }) => (
+        <ContactsElement key={id}>
+          <div>
+            <p>{name}:</p>
+            <p>{number}</p>
+          </div>
+          <DeleteButton onClick={() => onDeleteContact(id)}>
+            Delete contact
+          </DeleteButton>
+        </ContactsElement>
+      ))}
+    </ul>
+  );
+};
 
 export default Contacts;
