e096fa0bfdb14f4fffe30c8e567185f7ef416023
Dmitriy-Marchuk <93228534+Dmitriy-Marchuk@users.noreply.github.com>
2023-06-08 16:16:57 +0000

    Deploying to gh-pages from @ Dmitriy-Marchuk/goit-react-hw-08-phonebook-2023@7e1d2b7195356b85c47ce9878e37dae2283b8958 🚀

---
+++ static/js/415.f1e414ca.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook_2023=self.webpackChunkgoit_react_hw_08_phonebook_2023||[]).push([[415],{5415:function(e,n,o){o.r(n);var t=o(4270),c=o(184);n.default=function(){return(0,c.jsxs)(c.Fragment,{children:[(0,c.jsx)(t.q,{children:(0,c.jsx)("title",{children:"Home"})}),(0,c.jsx)("div",{children:"Phonebook welcome page! "})]})}}}]);
+//# sourceMappingURL=415.f1e414ca.chunk.js.map
